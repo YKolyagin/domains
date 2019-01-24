54be2c78e39ee79866950c42c877afc255ec2e4c
YKolyagin <rek2270611@gmail.com>
2019-01-25 00:20:25 +0300

    Домашнее задание к уроку 4 Сделал вроде все кроме 2 задания не хватило времени разобраться с ресайзом.

--- Homework1/index.php
+++ Homework1/index.php
@@ -5,22 +5,48 @@
   <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <style>
+    img {
+        height: 290px;
+        width: 390px;
+        margin: 5px;
+    }
+
+    div {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
+
+    form {
+        display: flex;
+        justify-content: center;
+    }
+
+    p {
+        color: red;
+        font-size: 16px;
+    }
+  </style>
   <title>Document</title>
 </head>
 <body>
+<form action="index.php" enctype="multipart/form-data" method="POST">
+    <input type="hidden" name="MAX_FILE_SIZE" value="300000000" />
+    <input name="userFile" type="file" />
+    <input type="submit" value="Отправить файл" />
+</form>
 <?php
-$a = rand(-15, 15);
-$b = rand(-15, 15);
-    echo "$a <br>";
-    echo "$b <br>";
-
-if ($a >= 0 && $b >= 0) {
-    echo "\$a и \$b положительные, выводим их разность " . ($a - $b);
-} elseif ($a < 0 && $b < 0) {
-    echo "\$a и \$b отрицательные, выводим их произведение " . ($a * $b);
-} else {
-    echo  "\$a и \$b разных знаков, выводим их сумму " . ($a + $b);
-}
+include "photo.php";
 ?>
+<div>
+    <?php
+    echo $error;
+    ?>
+</div>
+<div><?php
+    echo $linkPhoto;
+    ?>
+</div>
 </body>
 </html>
